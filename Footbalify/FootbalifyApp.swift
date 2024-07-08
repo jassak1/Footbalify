f66ccb19f30fc9c597324cca56cefcd97c886575
jassak <jassak@me.com>
2024-07-08 16:41:11 +0200

    Initial Commit

---
+++ Footbalify/FootbalifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootbalifyApp.swift
+//  Footbalify
+//
+//  Created by Adam Jassak on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FootbalifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
